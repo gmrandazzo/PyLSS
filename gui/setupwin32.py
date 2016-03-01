9f1416d6b31352bb4a05e4af80183154d203776e
Giuseppe Marco Randazzo <gmrandazzo@gmail.com>
2016-03-01 16:14:38 +0100

    Updated win32 build

--- gui/setupwin32.py
+++ gui/setupwin32.py
@@ -3,12 +3,13 @@ import py2exe
 import sys
 import os
 import matplotlib
+import FileDialog
 
 sys.path.append("C:\\Windows\\winsxs\\x86_microsoft.vc90.crt_1fc8b3b9a1e18e3b_9.0.30729.4940_none_50916076bcb9a742")
 
 
 setup(
     data_files = matplotlib.get_py2exe_datafiles(),
-    options = {'py2exe': {'bundle_files': 1, 'compressed': True}},
+    options = {'py2exe': {'bundle_files': 3, 'compressed': True, 'packages': ['FileDialog']}},
     windows = [{'script': 'pylss-gui.py'}],
     zipfile = None)
